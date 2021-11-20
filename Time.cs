55fcbfc54f5dc9c4cdae02663e2597fefd307255
gsgol <77744037+gsgol@users.noreply.github.com>
2021-11-20 22:54:56 +0300

    Update Time.cs

--- Time.cs
+++ Time.cs
@@ -6,7 +6,8 @@ public class DataMaster : MonoBehaviour
 {
     DateTime currentDate;
     DateTime oldDate;
-    void Start()
+	public static long time_difference;
+    void Set_Time_Difference()
     {
         //Store the current time when it starts
         currentDate = System.DateTime.Now;
@@ -16,19 +17,63 @@ public class DataMaster : MonoBehaviour
 
         //Convert the old time from binary to a DataTime variable
         DateTime oldDate = DateTime.FromBinary(temp);
-        print("oldDate: " + oldDate);
 
         //Use the Subtract method and store the result as a timespan variable
         TimeSpan difference = currentDate.Subtract(oldDate);
-        print("Difference: " + difference);
 
+		time_difference = Convert.ToInt64(difference);
     }
 
     void OnApplicationQuit()
     {
         //Savee the current system time as a string in the player prefs class
         PlayerPrefs.SetString("sysString", System.DateTime.Now.ToBinary().ToString());
-
-        print("Saving this date to prefs: " + System.DateTime.Now);
     }
+	bool Is_Possible(string name)
+	{
+		List<string> keyList = new List<string>(this.recipes[name].ingredients.Keys);
+		foreach (string ingredient in keyList)
+		{
+			if (this.warehouse[ingredient] == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	
+	}
+
+	void OldPrepareAnOrder(string name)
+	{
+		List<string> keyList = new List<string>(this.recipes[name].ingredients.Keys);
+
+		foreach (string ingredient in keyList)
+		{
+			this.warehouse[ingredient] -= 1;
+		}
+		money += this.recipes[name].price;
+	}
+	
+	void Offline_Orders()
+	{
+		Random rnd = new Random();
+		int num_of_clients = rnd.Next(time_difference / (max_interval * 2), time_difference / max_interval);
+		for (int i = 0; i < num_of_clients; ++i)
+		{
+			string name = MakeAnOrder();
+			if(Is_Possible(name))
+			{
+				OldPrepareAnOrder(name);
+			}
+			else
+			{
+				break;
+			}
+
+
+		}
+	}
+
+
+
 }
